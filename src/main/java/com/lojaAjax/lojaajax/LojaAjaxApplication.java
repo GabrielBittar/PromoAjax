e57a571d9b9a0cf9b6abd3218335d2a29b5ea3b3
GabrielBittar <gabrielbittardomingues@gmail.com>
2020-01-13 23:07:23 -0400

    Comeca a configurar

---
+++ src/main/java/com/lojaAjax/lojaajax/LojaAjaxApplication.java
@@ -0,0 +1,13 @@
+package com.lojaAjax.lojaajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaAjaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojaAjax/lojaajax/LojaAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaAjax.lojaajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
